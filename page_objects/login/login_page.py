dedf4f799b7786883e7cb683701ec22616b13203
Sachin Patil <sachin.patil@medwing.com>
2022-05-24 09:45:22 +0200

    first commit

---
+++ page_objects/login/login_page.py
@@ -0,0 +1,5 @@
+EMAIL_XPATH = "//input[@name='email']"
+PASSWORD_XPATH = "//input[@name='PASSWORD']"
+REMEMBER_ME_XPATH = "//input[@name='rememberMe']"
+PASSWORD_RECOVERY_XPATH = "//a[@href='/password-recovery']"
+LOGIN_BTN_XPATH = "//button[@type='submit']"
